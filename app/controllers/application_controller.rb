1c35abe7c14556426be5ab8f12746eecd3618d5f
kyota1210 <kytmbmp1210@gmail.com>
2020-09-24 18:02:04 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  berore_action :authenticate_user!
-  before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
-    
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
