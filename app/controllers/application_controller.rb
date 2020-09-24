997787551329e0351ef4224eea417abadc034e30
kyota1210 <kytmbmp1210@gmail.com>
2020-09-24 17:42:50 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  berore_action :authenticate_user!
+  before_action :configre_permitted_parameters, if: :devise_controller?
+
+    protected
+    
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
